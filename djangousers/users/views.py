e3f16f831c98ff89387a8cd0a118eb18a170b555
Damian Zbrożek <damianzbrozek@hotmail.com>
2021-11-16 12:26:51 +0100

    feat: export users to csv

--- djangousers/users/views.py
+++ djangousers/users/views.py
@@ -1,7 +1,12 @@
+import csv
+
+from django.http import HttpRequest, HttpResponse
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from users.forms import CreateUserForm
 from users.models import User
+from users.templatetags.user_extras import bizz_fuzz, is_allowed
 
 
 class UserListView(ListView):
@@ -31,3 +36,27 @@ class UserCreateView(CreateView):
     template_name_suffix = '_create_form'
     form_class = CreateUserForm
     success_url = reverse_lazy('user-list')
+
+
+class UserExportView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        users = User.objects.all()
+        response = HttpResponse(
+            content_type='text/csv',
+            headers={'Content-Disposition': 'attachment; filename="users.csv"'},
+        )
+
+        writer = csv.writer(response)
+        writer.writerow(['Username', 'Birthday', 'Eligible', 'Random Number', 'BizzFuzz'])
+        for user in users:
+            writer.writerow(
+                [
+                    user.username,
+                    user.birthday.strftime('%d/%m/%Y'),
+                    is_allowed(user.birthday),
+                    user.random_number,
+                    bizz_fuzz(user.random_number),
+                ]
+            )
+
+        return response
